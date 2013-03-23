98d42b90c22fcd96e6791304cb48919618e07ac8
sararob <sararob@brandeis.edu>
2013-03-23 09:09:30 -0700

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+  
+  def mozart
+  end
+  
+  def beethoven
+  end
+  
+  def bach
+  end
+  
+  def brahms
+  end
+  
+  def handel
+  end
+  
+  def stravinsky
+  end
+  
+  def tchaikovsky
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classicaltweets::Application.config.secret_token = 'fbf10a5205bfd7c2b56a7af81b4a6dccdfc625fa6e1265bb941b14be35d00109a07436ca188c8f9e702dd468085540ed06b98f0f89cb8f801201af498cd76356'
